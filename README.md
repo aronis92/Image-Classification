350cb96aed212a71bc60862d1bc740fc230e481f
Antonis Mand <antonis.mand@yahoo.com>
2019-08-29 01:57:46 +0300

    AlexNet

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Image-Classification
+
+
+| Model | Image Size | Epochs | Batch Size | Accuracy |
+|---|---|---|---|---|
+| LeNet | 256 | 70 | 64 | 94% |
+| AlexNet | 128 | 70 | 32 | 94% |
+
+
+
+![model=Lenet&img_size=256&epochs=70&batch_size=64](https://github.com/DelphianCalamity/Image-Classification/raw/master/results/model=Lenet&img_size=256&epochs=70&batch_size=64.png)
+![model=AlexNet&img_size=128&epochs=70&batch_size=32](https://github.com/DelphianCalamity/Image-Classification/raw/master/results/model=AlexNet&img_size=128&epochs=70&batch_size=32.png)
---
+++ models/AlexNet.py
@@ -0,0 +1,84 @@
+from keras.models import Sequential
+from keras.layers import Dense, BatchNormalization, Dropout
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers import Flatten
+from keras.callbacks import EarlyStopping
+
+
+def AlexNet(x_train, y_train, params):
+    model = Sequential()
+    model.add(
+        Conv2D(
+            filters=48,
+            activation="relu",
+            input_shape=(params["img_size"], params["img_size"], 3),
+            kernel_size=(3, 3),
+            padding="same",
+        )
+    )
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(
+        Conv2D(filters=128, activation="relu", kernel_size=(3, 3), padding="same")
+    )
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(BatchNormalization())
+    model.add(
+        Conv2D(filters=190, activation="relu", kernel_size=(3, 3), padding="same")
+    )
+    model.add(BatchNormalization())
+    model.add(
+        Conv2D(
+            filters=192,
+            activation="relu",
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+        )
+    )
+    model.add(BatchNormalization())
+    model.add(
+        Conv2D(
+            filters=128,
+            activation="relu",
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            padding="valid",
+        )
+    )
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(BatchNormalization())
+
+    model.add(Flatten())
+    model.add(Dense(2048, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Dense(2048, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Dense(500, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.4))
+
+    model.add(Dense(8, activation="softmax"))
+    model.summary()
+
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+    # es = EarlyStopping(mode="min", verbose=1, patience=10)
+    out = model.fit(
+        x_train,
+        y_train,
+        validation_split=0.1,
+        epochs=params["epochs"],
+        batch_size=params["batch_size"],
+        verbose=1,
+        shuffle=True,
+        # callbacks=[es],
+    )
+
+    return out, model
